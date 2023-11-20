4d6acc8f9de9cdf5130410bdf659b3c1fabc3906
tomcscscs <bright.chan88@gmail.com>
2023-11-20 13:25:56 +0900

    현재 자동로그인유지하는 중

--- src/main/java/data/KeepTickets.java
+++ src/main/java/data/KeepTickets.java
@@ -1,5 +1,45 @@
 package data;
 
+import java.sql.Date;
+
 public class KeepTickets {
+	String code;
+	String userId;
+	Date expiredAt;
+
+	public KeepTickets() {
+		super();
+	}
+
+	public KeepTickets(String code, String userId, Date expiredAt) {
+		super();
+		this.code = code;
+		this.userId = userId;
+		this.expiredAt = expiredAt;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public Date getExpiredAt() {
+		return expiredAt;
+	}
+
+	public void setExpiredAt(Date expiredAt) {
+		this.expiredAt = expiredAt;
+	}
 
 }
--- src/main/java/processor/KeepTicketsController.java
+++ src/main/java/processor/KeepTicketsController.java
@@ -1,5 +1,65 @@
 package processor;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import data.KeepTickets;
+
 public class KeepTicketsController {
+	public boolean save(KeepTickets one) throws ClassNotFoundException {
+		boolean result = false;
+		// 1. 데이터 베이스 연결
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		try (Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@43.201.68.36:1521:xe", "homekeeper",
+				"oracle")) {
+			// 2. 필요한 작업요청을 전송하고 응답을 받으면 됨.
+			String sql = "INSERT INTO keep_tickets VALUES(?, ?, ?)";
+			PreparedStatement pst = conn.prepareStatement(sql);
+
+			pst.setString(1, one.getCode());
+			pst.setString(2, one.getUserId());
+			pst.setDate(3, one.getExpiredAt());
+
+			int n = pst.executeUpdate(); // 요청 전송하고 DB에서 응답을 받아옴.
+			if (n == 1) {
+				result = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public KeepTickets findById(String id) throws ClassNotFoundException {
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+
+		try (Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@43.201.68.36:1521:xe", "homekeeper",
+				"oracle")) {
+			String sql = "SELECT * FROM USERS WHERE ID=?";
+
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, id);
+
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				KeepTickets one = new KeepTickets();
+
+				one.setCode(rs.getString("code"));
+				one.setUserId(rs.getString("user_id"));
+				one.setExpiredAt(rs.getDate("expired_at"));
+				return one;
+
+			} else {
+				return null;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
 
 }
