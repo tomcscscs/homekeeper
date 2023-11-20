f991f1f5faec6650741e21d0c11759d77ab50ad7
tomcscscs <bright.chan88@gmail.com>
2023-11-21 00:11:49 +0900

    logout

---
+++ src/main/java/processor/LogoutController.java
@@ -0,0 +1,27 @@
+package processor;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LogoutController extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getSession().invalidate();
+		
+		Cookie cookie = new Cookie("ticketCode", null);
+		cookie.setPath(req.getServletContext().getContextPath());
+		cookie.setMaxAge(0);
+		
+		
+		
+		
+	}
+	
+
+}
